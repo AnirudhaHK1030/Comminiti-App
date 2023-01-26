849c752fa6bba52095fa8f6eb3ea8fdcbfa6b7b2
Anirudha <anirudha@iastate.edu>
2023-01-26 14:57:36 -0600

    Created a basic login-authentication system for the comminiti app

---
+++ Flutter Chat App/chat_app/lib/pages/auth/register_page.dart
@@ -0,0 +1,167 @@
+import 'package:chat_app/pages/auth/login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:chat_app/widgets/widgets.dart';
+import 'package:flutter/gestures.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final FormKey = GlobalKey<FormState>();
+  String email = "";
+  String password = "";
+  String fullName = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   backgroundColor: Theme.of(context).primaryColor,
+      // ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
+          child: Form(
+            key: FormKey,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Comminiti",
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    "Create an account now to talk about the things you love!",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
+                  ),
+                  Image.asset("assets/login.png"),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: textInputDecoration.copyWith(
+                      labelText: "FullName",
+                      prefixIcon: Icon(
+                        Icons.person,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                    onChanged: (val) {
+                      setState(() {
+                        fullName = val;
+                      });
+                    },
+                    validator: (val) {
+                      if (val!.isEmpty) {
+                        return "Name cannot be empty";
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  TextFormField(
+                    decoration: textInputDecoration.copyWith(
+                      labelText: "Email",
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                    onChanged: (val) {
+                      setState(() {
+                        email = val;
+                      });
+                    },
+
+                    //check validation
+
+                    validator: (val) {
+                      return RegExp(
+                                  r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                              .hasMatch(val!)
+                          ? null
+                          : "Please enter a valid email";
+                    },
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: textInputDecoration.copyWith(
+                      labelText: "Password",
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                    validator: (val) {
+                      if (val!.length < 6) {
+                        return "Password must be atleast 6 characters";
+                      } else {
+                        return null;
+                      }
+                    },
+                    onChanged: (val) {
+                      setState(() {
+                        password = val;
+                      });
+                    },
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Theme.of(context).primaryColor,
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          )),
+                      child: const Text(
+                        "Register",
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                      onPressed: () {
+                        register();
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text.rich(TextSpan(
+                    text: "Already have an account?",
+                    style: TextStyle(color: Colors.black, fontSize: 14),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: "Login Now",
+                        style: TextStyle(
+                            color: Colors.black,
+                            decoration: TextDecoration.underline),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            nextScreen(context, LoginPage());
+                          },
+                      )
+                    ],
+                  ))
+                ]),
+          ),
+        ),
+      ),
+    );
+  }
+
+  register() {}
+}
---
+++ Flutter Chat App/chat_app/lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:72697915997:web:735137c899d3be174c7bb4";
+  static String apiKey = "AIzaSyAQSvcBgzzHnV_HqkICMeiX6U8jprIPZSk";
+  static String messagingSenderId = "72697915997";
+  static String projectId = "chatappflutter-550d8";
+  final primaryColor = const Color(0xFFee7b64);
+}
